4b1801e9ad3208ec414993ed1bb64ce6f6fb74fa
alex45101 <aleontiev00@gmail.com>
2026-02-24 13:48:39 -0800

    Started working on parser structure WIP

---
+++ CompilerTests/ParserTests.cs
@@ -0,0 +1,18 @@
+﻿using JavaWhoCompiler;
+using System.Linq.Expressions;
+
+namespace CompilerTests
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void EmptyTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("");
+            
+            AST root = Parser.Parse(tokens);
+
+            Assert.Null(root); //maybe should not return null but an empty
+        }
+    }
+}
---
+++ JavaWhoCompiler/Parser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace JavaWhoCompiler
+{
+    public abstract class AST
+    {
+        public AST[] Nodes { get; init; }
+
+        public AST(int nodeAmount)
+        {
+            Nodes = new AST[nodeAmount];
+        }
+    }
+
+    public class PrimaryExpression : AST
+    {
+        public PrimaryExpression(int nodeAmount) : base(nodeAmount)
+        {
+        }
+    }
+
+    public class CallExpression : AST
+    {
+        public CallExpression(int nodeAmount) : base(nodeAmount)
+        {
+        }
+    }
+
+    public class MultiplyExpression : AST
+    {
+        public MultiplyExpression(int nodeAmount) : base(nodeAmount)
+        {
+        }
+    }
+
+    public class AddExpresion : AST
+    {
+        public AddExpresion(int nodeAmount) : base(nodeAmount)
+        {
+        }
+    }
+
+    public class ComparisonExpression : AST
+    {
+        public ComparisonExpression(int nodeAmount) : base(nodeAmount)
+        {
+        }
+    }
+
+    public class EqualityExpression : AST
+    {
+        public EqualityExpression(int nodeAmount) : base(nodeAmount)
+        {
+        }
+    }
+
+    public class Expression : AST
+    {
+        public Expression(int nodeAmount) : base(nodeAmount)
+        {
+
+        }
+    }
+
+
+    public static class Parser
+    {
+        public static AST Parse(IEnumerable<IToken> tokens)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
