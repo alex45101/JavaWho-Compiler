a8bebbe008ce1460e3fde9b09b0d42b37c441e62
alex45101 <aleontiev00@gmail.com>
2026-02-23 17:27:56 -0800

    Added simple unit test for tokenizer, should have token test with test code examples

---
+++ CompilerTests/TokenizerTests.cs
@@ -0,0 +1,256 @@
+﻿using JavaWhoCompiler;
+
+namespace CompilerTests
+{
+    public class TokenizerTests
+    {
+        [Theory]
+        [InlineData("_x")]
+        [InlineData("x")]
+        [InlineData("x_")]
+        [InlineData("small_num")]
+        [InlineData("while_text")]
+        [InlineData("Int")]
+        [InlineData("Boolean")]
+        [InlineData("Cat")]
+        [InlineData("cat09")]
+        public void IdentifierTokensTest(string text)
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize(text);
+
+            Assert.IsType<IdentifierToken>(tokens.First());
+        }
+
+        [Fact]
+        public void NoTokensTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("");
+
+            Assert.Empty(tokens);
+        }
+
+        [Fact]
+        public void WhiteSpaceTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("     ");
+
+            Assert.IsType<WhiteSpaceToken>(tokens.First());
+        }
+
+        [Fact]
+        public void StringTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("\"Hello World!\"");
+
+            Assert.IsType<StringToken>(tokens.First());
+        }
+
+        [Fact]
+        public void NumberTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("425");
+
+            Assert.IsType<NumberToken>(tokens.First());
+        }
+
+        [Fact]
+        public void ThisTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("this");
+
+            Assert.IsType<ThisToken>(tokens.First());
+        }
+
+        [Fact]
+        public void TrueTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("true");
+
+            Assert.IsType<TrueToken>(tokens.First());
+        }
+
+        [Fact]
+        public void FalseTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("false");
+
+            Assert.IsType<FalseToken>(tokens.First());
+        }
+
+        [Fact]
+        public void NewTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("new");
+
+            Assert.IsType<NewToken>(tokens.First());
+        }
+
+        [Fact]
+        public void InitTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("init");
+
+            Assert.IsType<InitToken>(tokens.First());
+        }
+
+        [Fact]
+        public void ClassTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("class");
+
+            Assert.IsType<ClassToken>(tokens.First());
+        }
+
+        [Fact]
+        public void SuperTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("super");
+
+            Assert.IsType<SuperToken>(tokens.First());
+        }
+
+        [Fact]
+        public void WhileTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("while");
+
+            Assert.IsType<WhileToken>(tokens.First());
+        }
+
+        [Fact]
+        public void BreakTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("break");
+
+            Assert.IsType<BreakToken>(tokens.First());
+        }
+
+        [Fact]
+        public void IfTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("if");
+
+            Assert.IsType<IfToken>(tokens.First());
+        }
+
+        [Fact]
+        public void ElseTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("else");
+
+            Assert.IsType<ElseToken>(tokens.First());
+        }
+
+        [Fact]
+        public void VoidTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("void");
+
+            Assert.IsType<VoidToken>(tokens.First());
+        }
+
+        [Fact]
+        public void AddOperatorTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("+");
+
+            Assert.IsType<AddOperatorToken>(tokens.First());
+        }
+
+        [Fact]
+        public void SubtractOperatorTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("-");
+
+            Assert.IsType<SubtractOperatorToken>(tokens.First());
+        }
+
+        [Fact]
+        public void MultiplyOperatorTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("*");
+
+            Assert.IsType<MultiplyOperatorToken>(tokens.First());
+        }
+
+        [Fact]
+        public void DivideOperatorTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("/");
+
+            Assert.IsType<DivideOperatorToken>(tokens.First());
+        }
+
+        [Fact]
+        public void LessThanOperatorTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("<");
+
+            Assert.IsType<LessThanOperatorToken>(tokens.First());
+        }
+
+        [Fact]
+        public void EqualsOperatorTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("==");
+
+            Assert.IsType<EqualsOperatorToken>(tokens.First());
+        }
+
+        [Fact]
+        public void NotEqualsOperatorTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("!=");
+
+            Assert.IsType<NotEqualsOperatorToken>(tokens.First());
+        }
+
+        [Fact]
+        public void AssignmentOperatorTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("=");
+
+            Assert.IsType<AssignmentOperatorToken>(tokens.First());
+        }
+
+        [Fact]
+        public void OpenParenthesisTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize("(");
+
+            Assert.IsType<OpenParenthesisToken>(tokens.First());
+        }
+
+        [Fact]
+        public void CloseParenthesisTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize(")");
+
+            Assert.IsType<CloseParenthesisToken>(tokens.First());
+        }
+
+        [Fact]
+        public void SemiColonTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize(";");
+
+            Assert.IsType<SemiColonToken>(tokens.First());
+        }
+
+        [Fact]
+        public void CommaTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize(",");
+
+            Assert.IsType<CommaToken>(tokens.First());
+        }
+
+        [Fact]
+        public void DotTokenTest()
+        {
+            IEnumerable<IToken> tokens = Tokenizer.Tokenize(".");
+
+            Assert.IsType<DotToken>(tokens.First());
+        }
+    }
+}
--- JavaWhoCompiler/Tokenizer.cs
+++ JavaWhoCompiler/Tokenizer.cs
@@ -1,12 +1,16 @@
-﻿namespace JavaWhoCompiler
+﻿using System.Text.RegularExpressions;
+
+namespace JavaWhoCompiler
 {
     public interface IToken
     {
         string Value { get; init; }
     }
 
+    public class InvalidTokenException(string Message) : Exception(Message);
 
-    //
+    public sealed record UnknownToken(string Value) : IToken;
+
     public sealed record IdentifierToken(string Value) : IToken;
     public sealed record WhiteSpaceToken(string Value) : IToken;
     public sealed record StringToken(string Value) : IToken;
@@ -38,6 +42,7 @@
     public sealed record BreakToken(string Value) : IToken;
     public sealed record IfToken(string Value) : IToken;
     public sealed record ElseToken(string Value) : IToken;
+    public sealed record VoidToken(string Value) : IToken;
     #endregion
 
     #region Punctuation
@@ -47,10 +52,93 @@
     public sealed record CloseCurlyBracketToken(string Value) : IToken;
     public sealed record SemiColonToken(string Value) : IToken;
     public sealed record CommaToken(string Value) : IToken;
-    public sealed record PeriodToken(string Value) : IToken;
+    public sealed record DotToken(string Value) : IToken;
     #endregion
+
+    public static class RegexPatterns
+    {
+        public static IReadOnlyDictionary<string, Func<string, IToken>> PatternToToken = new Dictionary<string, Func<string, IToken>>()
+        {
+
+            [@"\G\s+"] = value => new WhiteSpaceToken(value),
+
+            [@"\G\d+"] = value => new NumberToken(value),
+            [@"\G""(?:\\.|[^""\\])*"""] = value => new StringToken(value),
+
+            [@"\Gthis\b"] = value => new ThisToken(value),
+            [@"\Gtrue\b"] = value => new TrueToken(value),
+            [@"\Gfalse\b"] = value => new FalseToken(value),
+            [@"\Gnew\b"] = value => new NewToken(value),
+            [@"\Ginit\b"] = value => new InitToken(value),
+            [@"\Gclass\b"] = value => new ClassToken(value),
+            [@"\Gsuper\b"] = value => new SuperToken(value),
+            [@"\Gwhile\b"] = value => new WhileToken(value),
+            [@"\Gbreak\b"] = value => new BreakToken(value),
+            [@"\Gif\b"] = value => new IfToken(value),
+            [@"\Gelse\b"] = value => new ElseToken(value),
+            [@"\Gvoid\b"] = value => new VoidToken(value),
+
+            [@"\G^[a-zA-Z_][a-zA-Z0-9_]*"] = value => new IdentifierToken(value),
+
+            [@"\G\+"] = value => new AddOperatorToken(value),
+            [@"\G\-"] = value => new SubtractOperatorToken(value),
+            [@"\G\*"] = value => new MultiplyOperatorToken(value),
+            [@"\G\/"] = value => new DivideOperatorToken(value),
+            [@"\G\=="] = value => new EqualsOperatorToken(value),
+            [@"\G\!="] = value => new NotEqualsOperatorToken(value),
+            [@"\G\="] = value => new AssignmentOperatorToken(value),
+            [@"\G\<"] = value => new LessThanOperatorToken(value),
+
+            [@"\G\("] = value => new OpenParenthesisToken(value),
+            [@"\G\)"] = value => new CloseParenthesisToken(value),
+            [@"\G\{"] = value => new OpenCurlyBracketToken(value),
+            [@"\G\}"] = value => new CloseCurlyBracketToken(value),
+            [@"\G;"] = value => new SemiColonToken(value),
+            [@"\G,"] = value => new CommaToken(value),
+            [@"\G\."] = value => new DotToken(value)
+        };
+    }
 
     public static class Tokenizer
     {
+        public static IEnumerable<IToken> Tokenize(string code)
+        {
+            List<IToken> tokens = new List<IToken>();
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                KeyValuePair<Match, IToken> tokenMatch = new KeyValuePair<Match, IToken>(
+                    key: Match.Empty,
+                    value: new UnknownToken("")
+                );
+
+                foreach (KeyValuePair<string, Func<string, IToken>> pattern in RegexPatterns.PatternToToken)
+                {
+                    Regex regex = new Regex(pattern.Key);
+
+                    Match currMatch = regex.Match(code, i);
+
+                    if (currMatch.Length > 0)
+                    {
+                        tokenMatch = new KeyValuePair<Match, IToken>(
+                            key: currMatch,
+                            value: pattern.Value(currMatch.Value)
+                        );
+                        break;
+                    }
+                }
+
+                if (tokenMatch.Value is UnknownToken)
+                {
+                    throw new InvalidTokenException($"Invalid token starting at position: {i}");
+                }
+
+                tokens.Add(tokenMatch.Value);
+
+                i += tokenMatch.Key.Length - 1;
+            }
+
+            return tokens;
+        }
     }
 }
