706ec6260a3224eb044c7183e489f7b6278f82a3
tscott12331 <t.scott12331@gmail.com>
2026-03-16 21:26:37 -0700

    WIP create overarching type class can prevent cyclic inheritance

--- JavaWhoCompiler/TypeChecker.cs
+++ JavaWhoCompiler/TypeChecker.cs
@@ -37,31 +37,154 @@ namespace JavaWhoCompiler
         }
     }
 
-    public class MethodSignature {
+    public abstract class TypeBase(string name) {
+        public string Name { get; } = name;
+        public abstract bool IsSubtypeOf(TypeBase other);
 
+
+        // primitives
+        public static PrimitiveType IntPrimitive = new("Int");
+        public static PrimitiveType BooleanPrimitive = new("Boolean");
+        public static PrimitiveType VoidPrimitive = new("Void");
+
+        // built ins
+        public static ClassType ObjectBuiltIn = new(
+                "Object",
+                [], // vardecs
+                new Constructor([], null, []),
+                [], // methods
+                null // extending class
+                );
+
+        public static ClassType StringBuiltIn = new(
+                "String",
+                [],
+                new Constructor([], null, []),
+                [], // methods
+                ObjectBuiltIn // extending class
+                );
     }
 
-    public class ClassType(
-        string className,
-        ClassType extendingClassType,
-        List<VariableDeclaration> variableDeclarations,
-        Constructor constructor,
-        List<MethodSignature> methodSignatures
-        ) {
-        string ClassName { get; } = className;
-        ClassType ExtendingClassType { get; } = extendingClassType;
-        List<VariableDeclaration> VariableDeclarations { get; } = variableDeclarations;
-        Constructor Constructor { get; } = constructor;
-        List<MethodSignature> MethodSignatures { get; } = methodSignatures;
-        
-        public bool IsSubtypeOf(ClassType classType) {
-            if(classType.ClassName == ClassName) return true;
+    public class PrimitiveType(string name) : TypeBase(name) {
+        public override bool IsSubtypeOf(TypeBase other) {
+            return Equals(other);
+        }
+    }
 
-            if(ExtendingClassType is null) return false;
 
-            return ExtendingClassType.IsSubtypeOf(classType);
+    public class TypeList(IEnumerable<TypeBase> types) : List<TypeBase>(types) {
+
+        public static TypeList FromASTNodes(List<AST> variableDeclarations) {
+            return new TypeList(variableDeclarations.Select(vd => {
+                            throw new NotImplementedException();
+                            return TypeBase.IntPrimitive;
+                        }));
         }
 
+        public override bool Equals(Object other) {
+            return other is TypeList paramTypes &&
+                    this.SequenceEqual(paramTypes);
+        }
+
+        public override int GetHashCode() {
+            HashCode hashCode = new();
+            foreach(TypeBase paramType in this) {
+                hashCode.Add(paramType.GetHashCode());
+            }
+
+            return hashCode.ToHashCode();
+        }
+    }
+
+    public sealed record MethodSignature(string Name, TypeList Parameters, string ReturnType);
+
+    public class ClassType : TypeBase
+    {
+        ClassType ExtendingClassType { get; }
+        Constructor Constructor { get; }
+
+        HashSet<MethodSignature> MethodSignatures { get; } = new();
+
+        // name to type
+        Dictionary<string, string> LocalClassFields { get; } = new();
+
+
+        public ClassType(
+            ClassDefinition classDefinition,
+            ClassType extendingClassType
+            ) 
+            : this(classDefinition.Name.Value,
+                    classDefinition.VariableDeclarations,
+                    classDefinition.Constructor,
+                    classDefinition.MethodDefinitions,
+                    extendingClassType) {}
+
+
+        public ClassType(
+                string className,
+                List<AST> variableDeclarations,
+                AST constructor,
+                List<AST> methodDefinitions,
+                ClassType extendingClassType
+                )
+                : base(className)
+        {
+            ExtendingClassType = extendingClassType;
+
+            InitializeLocalFields(variableDeclarations);
+
+            Constructor = (Constructor)constructor;
+            InitializeMethodSignatures(methodDefinitions);
+        }
+        
+        public override bool IsSubtypeOf(TypeBase other) {
+            switch(other) {
+                case ClassType classType: {
+                    if(classType.Name == Name) return true;
+
+                    if(ExtendingClassType is null) return false;
+
+                    return ExtendingClassType.IsSubtypeOf(classType);
+                }
+
+                default:
+                  return false;
+            }
+        }
+
+        public bool CanAccessField(string fieldName) {
+            return LocalClassFields.ContainsKey(fieldName) ||
+                   (
+                    ExtendingClassType is not null &&
+                    ExtendingClassType.CanAccessField(fieldName)
+                   );
+        }
+
+        private void InitializeLocalFields(List<AST> variableDeclarations) {
+            foreach(VariableDeclaration vd in variableDeclarations) {
+                if(CanAccessField(vd.Var.Value)) {
+                    throw new TypeException($"Redelcaration of field {vd.Var.Value}");
+                }
+
+                LocalClassFields.Add(
+                        vd.Var.Value,
+                        vd.Type.Value
+                        );
+            }
+        }
+
+        private void InitializeMethodSignatures(List<AST> methodDefinitions) {
+            foreach(MethodDefinition md in methodDefinitions) {
+                MethodSignatures.Add(
+                        new MethodSignature(
+                            md.Name.Value,
+                            TypeList.FromASTNodes(md.Parameters),
+                            // md.Parameters.Select(vd => ((VariableDeclaration)vd).Type.Value).ToList(),
+                            md.ReturnType.Value
+                            )
+                        );
+            }
+        }
     }
 
     public class TypeChecker
@@ -82,15 +205,20 @@ namespace JavaWhoCompiler
         }
 
         private ClassType ConvertAndCheckClassType(string className, 
-                                                   Dictionary<string, ClassDefinition> definedClasses) 
+                                                   Dictionary<string, ClassDefinition> definedClasses,
+                                                   HashSet<string> workingTree) 
         {
+            if(workingTree.Contains(className)) {
+                // cyclic inheritance
+                throw new TypeException($"Class {className} is part of an inheritance cycle");
+            }
+
             if(ClassTypes.GetValueOrDefault(className, null) is ClassType classType) {
                 // already defined this ClassType
                 return classType;
             }
 
-            ClassDefinition classDefinition = null;
-            if(!definedClasses.TryGetValue(className, out classDefinition)) {
+            if(!definedClasses.TryGetValue(className, out ClassDefinition classDefinition)) {
                 throw new TypeException($"Class {className} is not defined");
             }
 
@@ -101,10 +229,20 @@ namespace JavaWhoCompiler
                     throw new TypeException($"Inherited class {extendsName} is not defined");
                 }
 
-                extendingClassType = ConvertAndCheckClassType(extendsName, definedClasses);
+                workingTree.Add(className);
+                extendingClassType = ConvertAndCheckClassType(extendsName, definedClasses, workingTree);
             }
 
+            ClassType currentClassType = new(
+                    className,
+                    extendingClassType,
+                    classDefinition.VariableDeclarations,
+                    classDefinition.Constructor,
 
+                    )
+            
+            
+            
             return null;
         }
 
@@ -123,13 +261,8 @@ namespace JavaWhoCompiler
 
             // second pass: convert and check validity of class
             foreach(ClassDefinition classdef in classes) {
-                if(ClassTypes.GetValueOrDefault(classdef.Name.Value, null) is not null) {
-                     // already defined
-                     continue;
-                }
-
-                ClassTypes.Add(classdef.Name.Value, ConvertAndCheckClassType(classdef.Name.Value, DefinedClasses));
-
+                HashSet<string> workingTree = new();
+                ClassTypes.Add(classdef.Name.Value, ConvertAndCheckClassType(classdef.Name.Value, DefinedClasses, workingTree));
             }
         }
 
